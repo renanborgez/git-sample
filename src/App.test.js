303a52348371b6af8a2d35bd55249d117385d2f1
Renan Garcia Borges <renan.garcia.borges@ibm.com>
2023-03-01 11:06:24 +0100

    Add roar like a tiger

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn git/i);
+  const linkElement = screen.getByText(/roar like a tiger/i);
   expect(linkElement).toBeInTheDocument();
 });
